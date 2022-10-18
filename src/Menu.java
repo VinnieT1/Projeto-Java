b94813aebfdcbae98f4f2dd71b9824a8b8c590a4
Vinícius Teixeira <92376175+VinnieT1@users.noreply.github.com>
2022-10-18 20:07:21 -0300

    Delete src directory

--- src/Menu.java
+++
@@ -1,6 +0,0 @@
-public interface Menu {
-    public String displayMainMenu();
-    public String displayProjectMenu();
-    public String displayActivityMenu();
-    public String displayUserMenu();
-}
--- src/UndoRedo.java
+++
@@ -1,6 +0,0 @@
-import java.util.ArrayList;
-
-public interface UndoRedo {
-    public void undo(ArrayList<Project> projects, ArrayList<Activity> activities, ArrayList<User> users);
-    public void redo(ArrayList<Project> projects, ArrayList<Activity> activities, ArrayList<User> users);
-}
